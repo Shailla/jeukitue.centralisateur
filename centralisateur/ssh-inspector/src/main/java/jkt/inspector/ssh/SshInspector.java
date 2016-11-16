3a7a8a9e37dc38991a6fd35f10cbdbc46b87c8e2
jvl <jeanclaude.vogel@orange.com>
2016-11-16 19:14:18 +0100

    Plateform inspector

--- centralisateur/ssh-inspector/src/main/java/jkt/inspector/ssh/SshInspector.java
+++ centralisateur/ssh-inspector/src/main/java/jkt/inspector/ssh/SshInspector.java
@@ -32,12 +32,16 @@ public class SshInspector {
 	// Paramètres sondes
 	private static final String SCRIPT = "script";
 
+	private static final String RESULT_BEGIN = "{result}";
+	private static final String RESULT_END = "{/result}";
+
 	public static void main(String[] args) {
 		try {
 			new SshInspector();
 		}
 		catch(final Exception exception) {
 			exception.printStackTrace(System.err);
+			System.err.flush();
 		}
 	}
 
@@ -47,10 +51,10 @@ public class SshInspector {
 		// Load platforms and probes properties
 		/* *************************************************** */
 
-		final List<Properties> pfs = loadPropertiesDir(new File("platforms"));
-		final List<Properties> probes = loadPropertiesDir(new File("probes"));
-		
-		final Map<String, Map<String, String>> results = new HashMap<>();
+		final List<Properties> pfs = loadPropertiesDir(new File("platforms"), "platforme");
+		final List<Properties> probes = loadPropertiesDir(new File("probes"), "sonde");
+
+		final Map<String, Map<String, String>> results = new HashMap<>();		
 
 
 		/* *************************************************** */
@@ -67,40 +71,60 @@ public class SshInspector {
 
 			final Map<String, String> pfResults = new HashMap<>();
 			results.put(pfName, pfResults);
+
+			System.out.println("");
 			
-			System.out.println("\nConnecting to '" + pfName + " (" + pfDescription + ")' avec " + pfHost + ":" + pfPort + "@" + pfLogin + ":" + pfPwd + "'");
+			System.out.println("=========================================================================================================================");
+			System.out.println("Connecting to '" + pfName + "' : '" + pfHost + ":" + pfPort + "@" + pfLogin + ":" + pfPwd + "'");
+			System.out.println("=========================================================================================================================");
+
+			Session session = null;
 
 			try {
-				final Session session = connect(pfHost, pfPort, pfLogin, pfPwd);
+				session = connect(pfHost, pfPort, pfLogin, pfPwd);
 
 				for(final Properties probe : probes) {
 					final String pbName = probe.getProperty(NAME);
 					final String pbDescription = probe.getProperty(DESCRIPTION);
 					final String pbScript = probe.getProperty(SCRIPT);
 
-					System.out.println("Exécution du script '" + pbName + " (" + pbDescription + ")' qui est '" + pbScript + "'");
+					System.out.println("-------------------------------------------------------------------------------------------------------------------------");
+					System.out.println("Executing '" + pbName + "' : '" + pbScript + "'");
 
 					try {
 						final String result = execute(session, pbScript);
-						final String lastLine = result.substring(result.lastIndexOf("\n"));
-						System.out.println("Dernière ligne du résultat : " + lastLine + "'");
+						final String finalResult = result.substring(result.lastIndexOf(RESULT_BEGIN) + RESULT_BEGIN.length(), result.lastIndexOf(RESULT_END));
+
+						pfResults.put(pbName, finalResult);
 						
-						pfResults.put(pbName, lastLine);
+						System.out.println("Résultats intermédiaire => final : '" + result + "' => '" + finalResult + "'");
 					}
 					catch(final Exception exception) {
-						System.err.println("Echec de connexion !");
+						System.err.println("Echec d'exécution !");
+						exception.printStackTrace(System.err);
+						System.err.flush();
+
+						pfResults.put(pbName, "Probe failed");
 					}
 				}
 			}
 			catch(final Exception exception) {
 				System.err.println("Echec de connexion !");
 				exception.printStackTrace(System.err);
+				System.err.flush();
 			}
+			finally {
+				if(session != null) {
+					session.disconnect();
+				}
+			}
+			
+			System.out.println("");
 		}
-		
-		csvExport(results);
-	}
 
+		csvExport(results, pfs, probes);
+	}
+	
 	private Session connect(final String host, final int port, final String login, final String pwd) throws Exception {
 		JSch jsch = new JSch();
 
@@ -136,6 +160,7 @@ public class SshInspector {
 		};
 
 		final Session session = jsch.getSession(login, host, port);
+		session.setConfig("StrictHostKeyChecking", "no");
 		session.setUserInfo(ui);
 		session.connect();
 
@@ -143,53 +168,32 @@ public class SshInspector {
 	}
 
 	private String execute(final Session session, final String script) throws Exception {
-		String result = "";
-		ChannelExec channel = (ChannelExec)session.openChannel("exec");
+		StringBuilder outputBuffer = new StringBuilder();
 
-		// X Forwarding
-		// channel.setXForwarding(true);
+		final ChannelExec channel = (ChannelExec)session.openChannel("exec");
 
-		channel.setCommand(script);
-		channel.setInputStream(System.in);
-		channel.setOutputStream(System.out);
 		channel.setErrStream(System.err);
 
+		channel.setCommand(script);
 		InputStream in = channel.getInputStream();
-
 
 		channel.connect();
 
-		byte[] tmp = new byte[10000];
-		
-		while(true) {
-			while(in.available() > 0){
-				int i = in.read(tmp, 0, tmp.length);
-				
-				if(i<0)
-					break;
-				
-				result = new String(tmp, 0, i);
-				System.out.print(new String(tmp, 0, i));
-			}
-			
-			if(channel.isClosed()){
-				if(in.available() > 0) continue; 
-				System.out.println("exit-status: " + channel.getExitStatus());
-				break;
-			}
-			
-			try {
-				Thread.sleep(1000);
-			}
-			catch(Exception exception){
-				exception.printStackTrace(System.err);
-			}
+		try {
+			Thread.sleep(1000);
 		}
-		
+		catch(Exception exception){
+			exception.printStackTrace(System.err);
+			System.err.flush();
+		}
+
+		while(in.available() > 0) {
+			outputBuffer.append((char)in.read());
+		}
+
 		channel.disconnect();
-		session.disconnect();
-		
-		return result;
+
+		return new String(outputBuffer);
 	}
 
 	/**
@@ -197,7 +201,7 @@ public class SshInspector {
 	 * @param dir directory in which are the properties files
 	 * @return list of the properties
 	 */
-	private List<Properties> loadPropertiesDir(final File dir) throws Exception {
+	private List<Properties> loadPropertiesDir(final File dir, final String object) throws Exception {
 		if(!dir.exists()) {
 			System.err.println("Hé ho, ton répertoire il existe pas '" + dir.getAbsolutePath() + "'");
 		}
@@ -219,7 +223,7 @@ public class SshInspector {
 			final Properties prop = loadPropertiesFile(file);
 			props.add(prop);
 
-			System.out.println("Plateforme chargée '" + file + "'");
+			System.out.println(object + " chargée '" + file + "'");
 		}
 
 		return props;
@@ -245,24 +249,68 @@ public class SshInspector {
 
 		return properties;
 	}
-	
-	private void csvExport(final Map<String, Map<String, String>> results) throws Exception {
-		final FileWriter writer = new FileWriter("results.csv");
-		
-		for(final String pfName : results.keySet()) {
-			final Map<String, String> pfResults = results.get(pfName);
-			
-			for(final String pbName : pfResults.keySet()) {
-				final String pbResult = pfResults.get(pbName);
-				writer.write(pfName);
-				writer.write(",");
-				writer.write(pbName);
-				writer.write(",");
-				writer.write(pbResult);
-				writer.write("\n");
-			}			
+
+	private void csvExport(final Map<String, Map<String, String>> results, final List<Properties> pfs, final List<Properties> probes) throws Exception {
+
+		/* *************************************************** */
+		// Prepare CSV organisation
+		/* *************************************************** */
+
+		final List<String> pfNames = new ArrayList<>();
+		final List<String> probeNames = new ArrayList<>();
+
+		for(final Properties pf : pfs) {
+			final String pfName = pf.getProperty(NAME);
+			pfNames.add(pfName);
+			System.out.println("pfName" + pfName);
 		}
+
+		for(final Properties probe : probes) {
+			final String pbName = probe.getProperty(NAME);
+			probeNames.add(pbName);
+			System.out.println("pbName" + pbName);
+		}
+
+
+		/* *************************************************** */
+		// CSV export
+		/* *************************************************** */
+
+		final FileWriter writer = new FileWriter("results.csv");
+
+		// Export platforms list
+		writer.write("PFS");
 		
+		for(final String pfName : pfNames) {
+			writer.write(";");
+			writer.write(pfName);
+		}
+
+		writer.write("\n");
+
+		// Export probes results
+		for(final String pbName : probeNames) {
+			writer.write(pbName);
+
+			for(final String pfName : pfNames) {
+				final Map<String, String> pbs = results.get(pfName);
+
+				writer.write(";");
+
+				final String pbResult = pbs.get(pbName);
+
+				if(pbResult != null) {
+					writer.write(pbResult);
+				}
+				else {
+					writer.write("-");
+				}
+			}
+			
+			writer.write("\n");
+		}
+
+		writer.flush();		
 		IOUtils.closeQuietly(writer);
 	}
 }
